3aa7c402766f807fda8a5be4a471eb2081f2b863
Garrett George <45802549+garrettmichaelgeorge@users.noreply.github.com>
2025-06-08 19:49:12 -0500

    chore: improve comments in system test

--- test/system/forecasts_test.rb
+++ test/system/forecasts_test.rb
@@ -1,16 +1,13 @@
 require "application_system_test_case"
 
 class ForecastsTest < ApplicationSystemTestCase
-   # When I visit the homepage
-   # And I fill in the address field with "1 Apple Park Way, Cupertino, CA 95014"
-   # And I click, "Get Weather"
-   # Then I should see the temperature in degrees F
-   test "visiting the index" do
-     visit root_url
+  # TODO: use mocks for external API calls
+  test "fetching the forecast" do
+    visit root_url
 
-     fill_in "Address", with: "1 Apple Park Way, Cupertino, CA 95014"
-     click_on("Get forecast")
+    fill_in "Address", with: "1 Apple Park Way, Cupertino, CA 95014"
+    click_on "Get forecast"
 
-     assert_text "°F"
-   end
+    assert_text "°F"
+  end
 end

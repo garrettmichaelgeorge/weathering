fd4ebbabf50671235f23662a07c57276da0c80d6
Garrett George <45802549+garrettmichaelgeorge@users.noreply.github.com>
2025-06-08 23:33:44 -0500

    feat: cache forecast by postal code

--- app/controllers/weathering/forecasts_controller.rb
+++ app/controllers/weathering/forecasts_controller.rb
@@ -3,31 +3,46 @@ class Weathering::ForecastsController < ApplicationController
     @forecast = Weathering::Forecast.new
     @forecast.address = forecast_params.dig(:weathering_forecast, :address)
 
-    if @forecast.address.present?
-      logger.debug { "Fetching geocode for #{@forecast.address}" }
-      geocode_results = Geocoder.search(@forecast.address)
-      geocode_result = geocode_results.first
-      logger.debug { "Got coordinates #{geocode_result.coordinates} at postal code #{geocode_result.postal_code}" }
+    return unless @forecast.address.present?
 
-      forecast = fetch_forecast(geocode_result.coordinates)
-      logger.debug "Fetched forecast #{forecast}"
-      @forecast.current_temperature = forecast["current"]["temperature_2m"]
-      @forecast.current_temperature_unit = forecast["current_units"]["temperature_2m"]
-      logger.debug { "Got current temperature #{@forecast.current_temperature_with_unit} at postal code #{geocode_result.postal_code}" }
-    end
+    logger.debug { "Fetching geocode for #{@forecast.address}" }
+    geocode_results = Geocoder.search(@forecast.address)
+    geocode_result = geocode_results.first
+
+    return unless geocode_result.present?
+
+    logger.debug { "Got coordinates #{geocode_result.coordinates} at postal code #{geocode_result.postal_code}, full address #{geocode_result.address}" }
+
+    forecast =
+      Rails.cache.fetch(geocode_result.postal_code, expires_in: 30.minutes) do
+        fetch_forecast(geocode_result.coordinates)
+      end
+
+    @forecast.current_temperature = forecast["current"]["temperature_2m"]
+    @forecast.current_temperature_unit = forecast["current_units"]["temperature_2m"]
+
+    logger.debug { "Got current temperature #{@forecast.current_temperature_with_unit} at postal code #{geocode_result.postal_code}" }
   end
 
   private
 
   def forecast_params
-    params.permit(weathering_forecast: [ :address ])
+    params.permit(weathering_forecast: [:address])
   end
 
   def fetch_forecast(coordinates)
     conn = Faraday.new(
       url: "https://api.open-meteo.com",
-      params: { latitude: coordinates[0], longitude: coordinates[1], daily: "temperature_2m_max,temperature_2m_min", current: "temperature_2m", timezone: "auto", forecast_days: 1, temperature_unit: "fahrenheit" },
-      headers: { "Content-Type" => "application/json" }
+      params: {
+        latitude: coordinates[0],
+        longitude: coordinates[1],
+        daily: "temperature_2m_max,temperature_2m_min",
+        current: "temperature_2m",
+        timezone: "auto",
+        forecast_days: 1,
+        temperature_unit: "fahrenheit"
+      },
+      headers: {"Content-Type" => "application/json"}
     ) do |builder|
       builder.request :json
       builder.response :json
